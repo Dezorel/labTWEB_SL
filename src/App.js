e9600d80aa042e5ac7cd4e198ff9b9a1e7d8fde1
Leonid <scorpion23997@gmail.com>
2022-04-10 16:45:24 +0300

    form with fields

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import Box from '@mui/material/Box';
 
 function App() {
   return (
@@ -8,8 +10,49 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Septefrati Leonid CR-193 </h1>
       </header>
+        <FormFiveField></FormFiveField>
+        <FormSevenField></FormSevenField>
     </div>
   );
 }
 
 export default App;
+
+function FullWidthTextField() {
+    const [input, setInput] = useState('');
+    return (
+        <Box
+            sx={{
+                width: 500,
+                maxWidth: '100%',
+            }}
+        >
+            <input value={input} onInput={e => setInput(e.target.value)}/>
+        </Box>
+    );
+}
+
+function FormFiveField() {
+    return (
+        <form>
+            <FullWidthTextField></FullWidthTextField>
+            <FullWidthTextField></FullWidthTextField>
+            <FullWidthTextField></FullWidthTextField>
+            <FullWidthTextField></FullWidthTextField>
+            <FullWidthTextField></FullWidthTextField>
+            <h1>End first 5 fields</h1>
+        </form>
+    );
+}
+
+function FormSevenField() {
+    return (
+        <form>
+            <FormFiveField></FormFiveField>
+            <FullWidthTextField></FullWidthTextField>
+            <FullWidthTextField></FullWidthTextField>
+            <h1>End first 7 fields</h1>
+        </form>
+    );
+}
+
