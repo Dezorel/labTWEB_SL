b2e5c381bfaab5bf2164e69e174e6ceba875d4c9
Leonid <scorpion23997@gmail.com>
2022-04-11 21:06:00 +0300

    create login pass

--- src/App.js
+++ src/App.js
@@ -6,10 +6,38 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Septefrati Leonid CR-193 </h1>
+        <h1 onClick={registration("leonid", "123")}> Septefrati Leonid CR-193 </h1>
+          <input type="text" onChange={(e) => localStorage.setItem("registerUser", e.target.value)}></input>
+          <input type="password" onChange={(e) => localStorage.setItem("registerPass", e.target.value)}></input>
+          <button onClick={checkIfExist}>Enter</button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function registration(username, password)
+{
+    localStorage.setItem("user", username);
+    localStorage.setItem("pass", password);
+}
+
+function checkIfExist()
+{
+    if (localStorage.getItem("user") == localStorage.getItem("registerUser"))
+    {
+        if (localStorage.getItem("pass") == localStorage.getItem("registerPass"))
+        {
+            alert("Добро пожаловать " + localStorage.getItem("user"))
+        }
+        else
+        {
+            alert("Неправильный логин или пароль")
+        }
+    }
+    else
+    {
+        alert("Неправильный логин или пароль")
+    }
+}
